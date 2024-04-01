565c6fcb5b3ecc1ebad89f0f7f45eaa2e6e33d56
steinm58 <123842650+steinm58@users.noreply.github.com>
2024-04-01 18:19:46 +0000

    added things for lab 10

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import logo from './smiley.svg';
 import './App.css';
 
+// book array (data structure)
+const books = [
+  { id: 1, title: 'Things We Never Got Over', read: true},
+  { id: 2, title: 'Caraval', read: true},
+  { id: 3, title: 'The Kiss Deception', read: false},
+  { id: 4, title: 'Shatter Me', read: false},
+  { id: 5, title: 'Book Lovers', read: true}
+]
+
+// title (component)
+function Title() {
+  return (
+    <p>
+    Welcome to my library!
+  </p>
+  );
+}
+
+// inventory key (component)
+function Key() {
+  return (
+    <div>
+      <p>Bookshelf Key</p>
+      <div className="Key">
+        <p className="Read">Read</p>
+        <p className="NotRead">Not Read</p>
+      </div>
+    </div>
+  );
+}
+
+// inventory display (component) (conditional in here)
+function Bookshelf() {
+  const listBooks = books.map(book =>
+    <li key={book.id}
+      style={{
+        color: book.read ? '#f28482' : '#f6bd60' 
+      }}
+    >
+      {book.title}
+    </li>);
+
+    return (
+      <>
+        <h3>Bookshelf</h3>
+        <ul>{listBooks}</ul>
+      </>
+    );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Have a great day!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title />
+        <Key />
+        <Bookshelf />
       </header>
     </div>
   );
